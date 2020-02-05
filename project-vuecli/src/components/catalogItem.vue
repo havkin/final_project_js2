57c0a61d85b15161fc539cd090322501e8fae775
Maxim Sozinov <max.sozinov@gmail.com>
2020-02-05 21:20:10 +0700

    catalogItem done

---
+++ project-vuecli/src/components/catalogItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="product-item">
+    <img :src="item.img" alt="some image"/>
+    <div class="desc">
+      <h3>{{ item.title}}</h3>
+      <p>{{ item.price}} $</p>
+      <button class="buy-btn" @click="$root.$refs.cart.addItemToDB(item)">Купить</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+   props: ['item'],
+};
+</script>
+
+<style>
+</style>
