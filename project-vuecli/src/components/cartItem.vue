e1f93249d3b1c97015dc1167a398388ba949d12f
Maxim Sozinov <max.sozinov@gmail.com>
2020-02-05 21:24:21 +0700

    cartItem done

---
+++ project-vuecli/src/components/cartItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cart-item">
+    <div class="product-bio">
+      <img :src="item.img" alt="some image"/>
+      <div class="product-desc">
+        <p class="product-title">{{ item.title }}</p>
+        <p class="product-quantity">Quantity: {{ item.quantity }}</p>
+        <p class="product-single-price">\${{ item.price }} each</p>
+      </div>
+    </div>
+    <div class="right-block">
+      <p class="product-price">\${{ item.quantity * item.price }}</p>
+      <button class="del-btn" @click="$root.$refs.cart.removeItemfromDB(item)">&times;</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"]
+};
+</script>
+
+<style>
+</style>
