bc51d7b3b352f0cd0673f330ded2e499baed52d8
Maxim Sozinov <max.sozinov@gmail.com>
2020-02-05 21:38:46 +0700

    error, filter added

---
+++ project-vuecli/src/components/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="server-error" v-if="$root.smthWrong">
+     We are sorry! Server is not responsible.
+   </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ project-vuecli/src/components/filter.vue
@@ -0,0 +1,21 @@
+<template>
+  <form action="#" class="search-form">
+    <input type="text" class="search-field" v-model.trim="value" />
+    <button class="btn-search" type="submit" @click="$root.$refs.catalog.filterList(value)">
+      <i class="fas fa-search"></i>
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: ""
+    };
+  }
+};
+</script>
+
+<style>
+</style>
