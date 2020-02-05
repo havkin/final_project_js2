ae66efa860a17e19ca99948c98621e0dde60985a
Maxim Sozinov <max.sozinov@gmail.com>
2020-02-05 21:48:20 +0700

    App done

--- project-vuecli/src/App.vue
+++ project-vuecli/src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <div class="logo">E-shop</div>
+      <div class="search-and-cart">
+        <search-filter></search-filter>
+        <div id="cart" class="cart">
+          <button class="btn-cart" type="button" @click="$refs.cart.showUp()">Корзина</button>
+          <cart ref="cart"></cart>
+        </div>
+      </div>
+    </header>
+    <main>
+      <server-error></server-error>
+      <catalog ref="catalog"></catalog>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import cart from "./components/cart";
+import catalog from "./components/catalog";
+import serverError from "./components/error";
+import searchFilter from "./components/filter";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      smthWrong: false
+    };
+  },
+  methods: {
+    getJson(url) {
+      return fetch(url)
+        .then(result => result.json())
+        .catch(err => {
+          this.smthWrong = true;
+          console.log(err);
+        });
+    },
+    putJson(url, data) {
+      return fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(response => response.json())
+        .catch(err => {
+          this.smthWrong = true;
+          console.log(err);
+        });
+    },
+    deleteJson(url, data) {
+      return fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(response => response.json())
+        .catch(err => {
+          this.smthWrong = true;
+          console.log(err);
+        });
+    }
+  },
   components: {
-    HelloWorld
+    cart,
+    catalog,
+    "server-error": serverError,
+    "search-filter": searchFilter
   }
-}
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
 </style>
